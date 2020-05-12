8ef750e86375c5abaa019ea5c8e5f9bbe9856d7d
kavyasriram-04 <kavyasingapore@gmail.com>
2020-05-12 09:56:10 +0530

    make it better

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const Datastore = require('nedb');
+const app = express();
+
+
+app.listen(3002, () => console.log('listening at 3002'));
+app.use(express.static('public'));
+app.set('view engine', 'ejs')
+
+app.use(express.json({limit : '1mb'}));
+
+const database = new Datastore('database.db');
+database.loadDatabase();
+
+
+app.post('/mydata', (request, response) =>{
+    const data = request.body;
+    console.log(data);
+    database.insert(data);
+});
+   
+app.get('/mydata', (request, response) => {
+    database.find({}, (err, data) => {
+        if(err){
+            response.end();
+            return;
+        }
+        response.json(data);
+    });
+});
+
